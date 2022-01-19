b2db3a742d231610c0760a32e694c36539f2724b
c2tm <cyork4839@gmail.com>
2022-01-19 11:28:35 -0500

    new commit

---
+++ main.js
@@ -0,0 +1,18 @@
+(function(){
+
+("use strict");
+
+document.querySelector('button').addEventListener('click', () => document.querySelector('img').src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy1fipya5CJwKgqM74ZA2Fh3Rx3JkbHATIrnyldTP_FcH6A_JV9ZjBpVWhDF9K4dUBQhg&usqp=CAU");
+
+
+
+
+
+
+
+
+
+
+
+
+})()
